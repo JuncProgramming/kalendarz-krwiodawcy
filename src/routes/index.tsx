b20b9cc85636fcdf5bb2f094bdd92087f706f592
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2025-10-29 11:04:17 +0100

    Modify the homepage to use the DonationCalculator

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,34 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { DonationCalculator } from '@/components/DonationCalculator';
 
 export const Route = createFileRoute('/')({
   component: HomePage,
-})
+});
 
 function HomePage() {
   return (
-   <>Kalendarz Krwiodawcy</>
-  )
+    <div className="space-y-12 max-w-5xl flex-col flex items-center">
+      <h1 className="text-5xl/12 text-center font-semibold text-zinc-700 p-8">
+        Twoja krew może ocalić <br></br> ludzkie{' '}
+        <span className="text-red-500/90">życie</span>. Pomóż innym. <br></br> Zostań
+        krwiodawcą.
+      </h1>
+
+      <div className="w-full p-6 bg-zinc-50 border border-zinc-200 rounded-lg shadow-md">
+        <h2 className="font-semibold text-center pb-8 text-xl text-zinc-600">
+          Pierwszą dotację masz za sobą? Sprawdź możliwy termin kolejnej.
+        </h2>
+        <DonationCalculator />
+      </div>
+
+      <div className="text-center">
+        <p className="text-lg text-zinc-600">
+          Jesteś tu pierwszy raz?{' '}
+          <Link to="/" className="font-semibold text-red-600 hover:underline">
+            Dowiedz się, jak zacząć oddawać krew (FAQ)
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
