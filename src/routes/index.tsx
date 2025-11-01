450c410245a82d44c9e4fdfcc6a877d69a13e5b7
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2025-11-01 11:50:17 +0100

    Adjust the heading's color and position

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,8 +10,8 @@ function HomePage() {
     <div className="space-y-12 max-w-5xl flex-col flex items-center">
       <h1 className="text-5xl/12 text-center font-semibold text-zinc-700 p-8">
         Twoja krew może ocalić <br className="hidden xl:block" /> ludzkie{' '}
-        <span className="text-red-500/90">życie</span>. Pomóż innym. <br className="hidden xl:block" /> Zostań
-        krwiodawcą.
+        <span className="text-red-600">życie</span>. Pomóż innym.{' '}
+        <br className="hidden xl:block" /> Zostań krwiodawcą.
       </h1>
 
       <div className="w-full p-6 bg-zinc-50 border border-zinc-200 rounded-lg shadow-md">
@@ -24,7 +24,9 @@ function HomePage() {
       <div className="text-center">
         <p className="text-lg text-zinc-600">
           Jesteś tu pierwszy raz?{' '}
-          <Link to="/faq" className="font-semibold text-red-600 hover:underline">
+          <Link
+            to="/faq"
+            className="font-semibold text-red-600 hover:underline">
             Dowiedz się, jak zacząć oddawać krew (FAQ)
           </Link>
         </p>
