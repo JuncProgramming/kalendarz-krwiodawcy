33d75b58432d8d6a0a3a1f52153b6b5a0d8e581e
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2025-11-04 19:54:26 +0100

    Add supabaseClient (setup)

---
+++ src/supabaseClient.ts
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Nie znaleziono zmiennych środowiskowych Supabase");
+}
+
+export const supabase = createClient<any>(supabaseUrl, supabaseAnonKey);
