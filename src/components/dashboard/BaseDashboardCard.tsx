f5aa4862a7db5fd561cd64475edd10a5fbdc420d
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2025-11-22 04:34:00 +0100

    Add a base dashboard card for most of the cards in the dashboard

---
+++ src/components/dashboard/BaseDashboardCard.tsx
@@ -0,0 +1,12 @@
+import { type BaseDashboardCardProps } from '@/types';
+
+export function BaseDashboardCard({ title, children }: BaseDashboardCardProps) {
+  return (
+    <section className="p-6 border border-zinc-200 shadow-sm rounded-lg bg-zinc-50 flex flex-col">
+      <div className="flex justify-between items-center mb-2">
+        <h3 className="text-xl font-semibold text-zinc-800">{title}</h3>
+      </div>
+      {children}
+    </section>
+  );
+}
