cd4c6e51256fd8e9569b15e99600f2674937ff6f
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2025-12-30 10:17:41 +0100

    Add vitest to dependencies and write the first test (utils)

---
+++ src/__tests__/utils.test.ts
@@ -0,0 +1,44 @@
+import { normalizeType } from '@/utils';
+import { describe, it, expect } from 'vitest';
+
+describe('utils', () => {
+  describe('normalizeType', () => {
+    it('should return "krew_pelna" when "Krew pełna" or "krew_pelna" is passed in', () => {
+      const result1 = normalizeType('Krew pełna');
+      const result2 = normalizeType('krew_pelna');
+
+      expect(result1).toBe('krew_pelna');
+      expect(result2).toBe('krew_pelna');
+    });
+
+    it('should return "osocze" when "Osocze" or "osocze" is passed in', () => {
+      const result1 = normalizeType('Osocze');
+      const result2 = normalizeType('osocze');
+
+      expect(result1).toBe('osocze');
+      expect(result2).toBe('osocze');
+    });
+
+    it('should return "plytki" when "Płytki krwi" or "plytki" is passed in', () => {
+      const result1 = normalizeType('Płytki krwi');
+      const result2 = normalizeType('plytki');
+
+      expect(result1).toBe('plytki');
+      expect(result2).toBe('plytki');
+    });
+
+    it('should return "krew_pelna" when an empty string is passed in', () => {
+      expect(normalizeType('')).toBe('krew_pelna');
+    });
+
+    it('should return "krew_pelna" when string other than the set strings is passed in', () => {
+      const result1 = normalizeType('random');
+      const result2 = normalizeType('abc');
+      const result3 = normalizeType('123');
+
+      expect(result1).toBe('krew_pelna');
+      expect(result2).toBe('krew_pelna');
+      expect(result3).toBe('krew_pelna');
+    });
+  });
+});
